d4ac9f44697c558bcba78d5623ba8938d57cc3bd
Muhammad Said <mohammedabdy10@gmail.com>
2025-11-17 12:52:24 +0300

    refactor: uncomment login test and related imports

--- tests/api/auth/login.api.setup.ts
+++ tests/api/auth/login.api.setup.ts
@@ -1,17 +1,17 @@
-// import { EMAIL, PASSWORD } from "../../../env";
-// import { expect, test } from "../../../fixtures";
-// import { httpStatusCodes } from "../../../helpers/utils";
+import { EMAIL, PASSWORD } from "../../../env";
+import { expect, test } from "../../../fixtures";
+import { httpStatusCodes } from "../../../helpers/utils";
 
-// test("User can login and list articles", async ({ auth, articles }) => {
-//   const loginResponse = await auth.login({
-//     user: { email: EMAIL, password: PASSWORD },
-//   });
+test("User can login and list articles", async ({ auth, articles }) => {
+  const loginResponse = await auth.login({
+    user: { email: EMAIL, password: PASSWORD },
+  });
 
-//   expect(loginResponse.body.user.token).toBeTruthy();
-//   expect(loginResponse.status).toBe(httpStatusCodes.ok);
+  expect(loginResponse.body.user.token).toBeTruthy();
+  expect(loginResponse.status).toBe(httpStatusCodes.ok);
 
-//   const list = await articles.list({ limit: 2 });
-//   expect(list.body.articles.length).toBeGreaterThan(0);
-// });
+  const list = await articles.list({ limit: 2 });
+  expect(list.body.articles.length).toBeGreaterThan(0);
+});
 
 
