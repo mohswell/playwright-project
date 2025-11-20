030fe1a87376a1208a086839134b04d1b017bbc0
mohswell <muhammad@solutech.co.ke>
2025-11-20 21:43:10 +0300

    feat: add faker utility functions file

---
+++ helpers/faker.ts
@@ -0,0 +1,29 @@
+import { faker } from "@faker-js/faker";
+import type { ArticleRequest } from "../types/schema";
+
+/**
+ * Generate a unique ArticleRequest payload for tests.
+ */
+export function generateArticleData(): ArticleRequest {
+  return {
+    article: {
+      title: `${faker.string.alphanumeric({ length: 8 })} - ${faker.lorem.words(
+        2
+      )}`,
+      description: faker.lorem.sentence(),
+      body: faker.lorem.paragraph(),
+      tagList: [faker.lorem.word(), faker.lorem.word(), faker.lorem.word()],
+    },
+  };
+}
+
+/**
+ * Generate a unique Comment for tests.
+ */
+export function generateCommentData() {
+  return {
+    comment: {
+      body: faker.lorem.sentence(),
+    },
+  };
+}
