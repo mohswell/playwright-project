a51d71c7541d4727d431feae5ee4ddddf1abbbd1
mohswell <muhammad@solutech.co.ke>
2025-11-20 20:09:17 +0300

    refactor: add tags and comments services to test fixtures

--- fixtures/api/api.core.ts
+++ fixtures/api/api.core.ts
@@ -3,11 +3,15 @@ import { API_URL } from "../../env";
 import { APIClient } from "../../services/api.service";
 import { ArticleService } from "../../services/articles.service";
 import { AuthService } from "../../services/auth.service";
+import { TagsService } from "../../services/tags.service";
+import { ArticleCommentsService as CommentsService } from "../../services/comments.service";
 
 type Fixtures = {
   api: APIClient;
   auth: AuthService;
   articles: ArticleService;
+  tags: TagsService;
+  comments: CommentsService;
 };
 
 export const test = base.extend<Fixtures>({
@@ -21,6 +25,12 @@ export const test = base.extend<Fixtures>({
   articles: async ({ api }, use) => {
     await use(new ArticleService(api));
   },
+  tags: async ({ api }, use) => {
+    await use(new TagsService(api));
+  },
+  comments: async ({ api }, use) => {
+    await use(new CommentsService(api));
+  }
 });
 
 export { expect };
