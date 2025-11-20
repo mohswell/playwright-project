3e0ebc79ba65d88e45bc634982cb31cb29e028b0
mohswell <muhammad@solutech.co.ke>
2025-11-20 18:49:38 +0300

    refactor: streamline global setup function by renaming and removing unused code

--- global.ts
+++ global.ts
@@ -1,32 +1,23 @@
 import { request } from "@playwright/test";
 import { API_URL, EMAIL, PASSWORD } from "./env";
 import { apiEndpoints, assertValue } from "./helpers/utils";
-import { LoginRequest, LoginResponse } from "./types/schema";
-import { AUTH_TOKEN_PATH } from "./types/constants";
+import type { LoginRequest, LoginResponse } from "./types/schema";
 
-async function globalTestSetup() {
-  const reqContext = await request.newContext();
-
+async function globalSetup() {
+  const req = await request.newContext();
   const loginData: LoginRequest = {
     user: { email: EMAIL, password: PASSWORD },
   };
+  const res = await req.post(`${API_URL}/${apiEndpoints.auth.login}`, {
+    data: loginData,
+  });
+  const body: LoginResponse = await res.json();
 
-  // use type for login requets and login response
-
-  const response = await reqContext.post(
-    `${API_URL}/${apiEndpoints.auth.login}`,
-    {
-      data: loginData,
-    }
+  process.env.API_TOKEN = assertValue(
+    body.user.token,
+    "Login failed, token missing"
   );
-
-  const body: LoginResponse = await response.json();
-  const token = body.user.token;
-
-  assertValue(token, "Login failed, token missing");
-
-  // Save token in Playwright storage state
-  await reqContext.storageState({ path: AUTH_TOKEN_PATH });
+  await req.dispose();
 }
 
-export default globalTestSetup;
+export default globalSetup;
