feff714ab93c1c916c4d9c30f2bc13f9be6859a2
mohswell <muhammad@solutech.co.ke>
2025-11-20 20:11:12 +0300

    feat: add initial test structure for Article Comments API

---
+++ tests/api/comments/comments.api.spec.ts
@@ -0,0 +1,8 @@
+import { test } from "../../../fixtures";
+
+test.describe("Article Comments", () => {
+  test("", { tag: "@API" }, async ({ comments }) => {
+    // Placeholder for future comment tests
+    
+  });
+});
