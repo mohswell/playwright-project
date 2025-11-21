a20403ef4a6c95c4abd4d71d6de98054a2290321
Muhammad Said <mohammedabdy10@gmail.com>
2025-11-21 12:51:29 +0300

    fix: update settings tests to use expect for visibility checks

--- tests/client/profile_settings/settings.spec.ts
+++ tests/client/profile_settings/settings.spec.ts
@@ -1,24 +1,26 @@
-import { test } from "@/fixtures";
-import { generateUserBio, generateUserImage } from "@/helpers/faker";
+import { expect, test } from '@/fixtures';
+import { generateUserBio, generateUserImage } from '@/helpers/faker';
 
 test.describe('Settings Functionality', () => {
+    test('should display the settings page', async ({
+        settingsPage,
+    }, testInfo) => {
+        testInfo.annotations.push({ type: 'tag', description: '@Smoke' });
 
-  test('should display the settings page', async ({ settingsPage }) => {
-    await test.step('Verify Navigation Settings Page', async () => {
-      await settingsPage.settingsButton.click();
-      await settingsPage.settingsPageTitle.isVisible();
+        await settingsPage.settingsButton.click();
+        await expect(settingsPage.settingsPageTitle).toBeVisible();
     });
-  });
 
-  test('should update the user profile settings successfully', async ({ settingsPage }) => {
-    await test.step('Verify User Profile Update', async () => {
-      await settingsPage.updateSettings({
-        profilePictureUrl: generateUserImage().profilePictureUrl,
-        bio: generateUserBio().bio
-      });
+    test('should update the user profile settings successfully', async ({
+        settingsPage,
+    }, testInfo) => {
+        testInfo.annotations.push({ type: 'tag', description: '@Smoke' });
+
+        await settingsPage.updateSettings({
+            profilePictureUrl: generateUserImage().profilePictureUrl,
+            bio: generateUserBio().bio,
+        });
+
+        await settingsPage.assertSettingsUpdated();
     });
-    await settingsPage.assertSettingsUpdated();
-  });
-
 });
-
