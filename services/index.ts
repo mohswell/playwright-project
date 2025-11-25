3430dd1297bf0560ea1e83c3d1e84dee15155e25
Muhammad Said <mohammedabdy10@gmail.com>
2025-11-25 11:55:21 +0300

    add barrel file for services folder

---
+++ services/index.ts
@@ -0,0 +1,7 @@
+export * from './articles.service';
+export * from './auth.service';
+export * from './comments.service';
+export * from './api.service';
+export * from './profile.service';
+export * from './tags.service';
+export * from './mixin';
