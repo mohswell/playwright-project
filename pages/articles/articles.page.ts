a7cb6c2749767d0ba2f8b3b2485bc29b6ab3bbdf
Muhammad Said <mohammedabdy10@gmail.com>
2025-11-25 11:30:44 +0300

    feat: implement ArticlePage class and add article creation tests

--- pages/articles/articles.page.ts
+++ pages/articles/articles.page.ts
@@ -1,5 +1,5 @@
-import { Page, Locator, expect } from "@playwright/test";
-
+import { Page, Locator, expect } from '@playwright/test';
+import { BasePage } from '../base.page';
 
 /**
  * This is the page object for the Articles functionality.
@@ -7,7 +7,101 @@ import { Page, Locator, expect } from "@playwright/test";
  * @class ArticlesPage
  */
 
-export class ArticlesPage {
-    constructor(private page: Page) {}
+export class ArticlePage extends BasePage {
+    constructor(page: Page) {
+        super(page);
+    }
 
+    getPage() {
+        return this.page;
+    }
+
+    /** Form inputs */
+    get form(): {
+        articleTitle: Locator;
+        articleAbout: Locator;
+        articleDescription: Locator;
+        articleTags: Locator;
+        articleSubmitButton: Locator;
+    } {
+        return {
+            articleTitle: this.page.getByRole('textbox', {
+                name: 'Article Title',
+            }),
+            articleAbout: this.page.getByRole('textbox', {
+                name: "What's this article about?",
+            }),
+            articleDescription: this.page.getByRole('textbox', {
+                name: 'Write your article (in markdown)',
+            }),
+            articleTags: this.page.getByRole('textbox', { name: 'Enter tags' }),
+            articleSubmitButton: this.page.getByRole('button', {
+                name: 'Publish Article',
+            }),
+        };
+    }
+
+    get actions(): {
+        publishArticle: Locator;
+    } {
+        return {
+            publishArticle: this.form.articleSubmitButton,
+        };
+    }
+
+    get articleView() {
+        return {
+            title: this.page.locator('.article-page h1'),
+
+            description: this.page.locator('.article-content p'),
+
+            author: this.page.locator('.article-meta .author').first(),
+
+            tags: this.page.locator('.tag-list .tag-default'),
+        };
+    }
+
+    /**
+     * Creates an article
+     * @param {string} params.title - The title of the article.
+     * @param {string} params.about - A brief description about the article.
+     * @param {string} params.description - The full content of the article.
+     * @param {string[]} params.tags - Tags associated with the article.
+     */
+
+    async createArticle(params: {
+        title: string;
+        about: string;
+        description: string;
+        tags: string[];
+    }): Promise<void> {
+        await super.navigateToArticlePage();
+        await this.form.articleTitle.fill(params.title);
+        await this.form.articleAbout.fill(params.about);
+        await this.form.articleDescription.fill(params.description);
+        await this.form.articleTags.fill(params.tags.join(', '));
+        await this.actions.publishArticle.click();
+    }
+
+    async assertArticleCreated(params: {
+        title: string;
+        description: string;
+        tags: string[];
+    }) {
+        // Wait for page title to show
+        await expect(this.articleView.title).toBeVisible();
+
+        await expect(this.articleView.title).toHaveText(params.title);
+        await expect(this.articleView.description).toContainText(
+            params.description
+        );
+
+        // Tags (the RealWorld app groups tags like "lorem, ipsum")
+        // So check each tag exists inside the <li> element
+        for (const tag of params.tags) {
+            await expect(
+                this.articleView.tags.filter({ hasText: tag })
+            ).toBeVisible();
+        }
+    }
 }
