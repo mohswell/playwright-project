0302cb34ecb7202dff5034044d83293c6cb87aa2
Muhammad Said <mohammedabdy10@gmail.com>
2025-11-25 15:09:51 +0300

    feat: enhance article functionality with comment tests

--- pages/articles/articles.page.ts
+++ pages/articles/articles.page.ts
@@ -20,6 +20,10 @@ export class ArticlePage extends BasePage {
         return this.page.locator('button:has(i.ion-heart)').first();
     }
 
+    getFirstArticle(): Locator {
+        return this.page.locator('a.preview-link:has(h1)').first();
+    }
+
     /** Form inputs */
     get form(): {
         articleTitle: Locator;
@@ -27,6 +31,8 @@ export class ArticlePage extends BasePage {
         articleDescription: Locator;
         articleTags: Locator;
         articleSubmitButton: Locator;
+        commentArticleInput: Locator;
+        commentSubmitButton: Locator;
     } {
         return {
             articleTitle: this.page.getByRole('textbox', {
@@ -42,14 +48,22 @@ export class ArticlePage extends BasePage {
             articleSubmitButton: this.page.getByRole('button', {
                 name: 'Publish Article',
             }),
+            commentArticleInput: this.page.getByRole('textbox', {
+                name: 'Write a comment...',
+            }),
+            commentSubmitButton: this.page.getByRole('button', {
+                name: 'Post Comment',
+            }),
         };
     }
 
     get actions(): {
         publishArticle: Locator;
+        postComment: Locator;
     } {
         return {
             publishArticle: this.form.articleSubmitButton,
+            postComment: this.form.commentSubmitButton,
         };
     }
 
@@ -110,6 +124,7 @@ export class ArticlePage extends BasePage {
     }
 
     async clickLike() {
+        await super.navigateToHomePage();
         const btn = this.getFavoriteButton();
 
         const countText = await btn.innerText();
@@ -118,5 +133,15 @@ export class ArticlePage extends BasePage {
         await btn.click();
 
         await expect(btn).toHaveText(String(initialCount + 1));
+    }
+
+    async navigateToFirstArticle() {
+        const firstArticle = this.getFirstArticle();
+        await firstArticle.click();
+    }
+
+    async commentOnArticle(comment: string): Promise<void> {
+        await this.form.commentArticleInput.fill(comment);
+        await this.actions.postComment.click();
     }
 }
--- tests/client/articles/articles.client.spec.ts
+++ tests/client/articles/articles.client.spec.ts
@@ -1,5 +1,5 @@
-import { test, expect } from '@/fixtures';
-import { generateRandomArticle } from '@/helpers/faker';
+import { test } from '@/fixtures';
+import { generateCommentData, generateRandomArticle } from '@/helpers/faker';
 
 test.describe('Articles functionality', () => {
     test(
@@ -22,6 +22,16 @@ test.describe('Articles functionality', () => {
         { tag: '@Smoke' },
         async ({ articlePage }) => {
             await articlePage.clickLike();
+        }
+    );
+
+    test(
+        'Users can comment on Articles successfully',
+        { tag: '@Smoke' },
+        async ({ articlePage }) => {
+            await articlePage.navigateToFirstArticle();
+            const commentText = generateCommentData().comment.body;
+            await articlePage.commentOnArticle(commentText);
         }
     );
 });
